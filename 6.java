2d86c09317b7e5fbeb36fa4c03971684b9cecedc
pasharafiac <60223908+pasharafiac@users.noreply.github.com>
2023-10-26 12:50:37 +0700

    Create 6.java

---
+++ 6.java
@@ -0,0 +1,8 @@
+public class LoopingAZ {
+    public static void main(String[] args) {
+        // Mencetak karakter dari 'A' hingga 'Z'
+        for (char c = 'A'; c <= 'Z'; c++) {
+            System.out.print(c + " ");
+        }
+    }
+}
